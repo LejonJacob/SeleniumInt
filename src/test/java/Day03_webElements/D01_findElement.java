3fa363ac6cd2b93107a53d601a56d5172f4445d4
LejonJacob <arrode06@gmail.com>
2022-10-15 02:10:11 +0200

    Lesson Update

--- src/test/java/Day03_webElements/D01_findElement.java
+++ src/test/java/Day03_webElements/D01_findElement.java
@@ -40,25 +40,31 @@ public class D01_findElement {
           bunlardan 6 tanesi HTML uzerindeki tag veya attribute'lere goredir
           2 tanesi ise daha spesifik olarak, ilk 6 tane locator ile bulamadigimiz
           webElementleri locate etmek icin kullanilir
+
           1- By.id
              id genelde unique verilir
              dolayisiyla bir HTML elementde id varsa once By.id denenebilir
+
           2- className
              class attribute'u genelde ayni islevi yapan webelementleri gruplandirmak icin kullanilir
              bu gruplandirmalar sayesinde css ile gruptaki tum webElementler tek yerden sekillendirilebilir
              class attribute'u kullanilirken eger class value'u space iceriyorsa,
              classname ile yapilan locate'ler hata verebilir
              genellikle classname ile yapilan locate'ler birden fazla webelement dondureceginden
+
              List<WebElement> seklinde olusturacagimiz bir List'e kaydolur
              Bu listedeki elementler WebElement oldugu icin
              bu liste direk yazdirilamaz,
              bunun yerine for-each loop kullanilarak webElement ozellikleri yazdirilabilir
+
            3- name
               eger HTML kodunda name attribute varsa kullanilabilir
+
            4- tagname
               tagname de classname gibi gruplandirmalar icin kullanilir
               dolayisiyla tagname ile genelde bir element degil
               benzer ozelliklerdeki bir listeye ulasilir
+
            5- linkText
               sadece link'ler icin kullanilir
               her link HTML elementinin uzerinde bir link yazisi olur
@@ -66,9 +72,13 @@ public class D01_findElement {
               ncak bazen space gibi farkedemeyecegimiz eklemeler olabilir
               locate ederken linktext kullanilirsa, link yazisinin tamami kullanilmalidir
               (varsa bosluk vb... locate alinirken yazilmalidir"
+
            6- partialLinkText
               linktext gibi sadece linkler icin kullanilir
               linktext'den farki, tum yaziyi degil parcasini kullanmamizin da yeterli olmasidir.
+
          */
+
+
     }
 }
